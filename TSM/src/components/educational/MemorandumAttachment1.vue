5d9c4e0612c56c85830f99904d097204f4e7f60f
LXX-WJZ <85142000+LXX-WJZ@users.noreply.github.com>
2021-06-25 01:33:17 +0800

    学员新增

--- TSM/src/components/educational/MemorandumAttachment1.vue
+++ TSM/src/components/educational/MemorandumAttachment1.vue
@@ -7,8 +7,8 @@
 				<el-option label="所有" value=""></el-option>
 				<el-option label="招生未审核" value="1"></el-option>
 				<el-option label="招生已审核" value="2"></el-option>
-				<!-- 				<el-option label="教务未审核" value="3"></el-option>
-				<el-option label="教务已审核" value="4"></el-option> -->
+				<el-option label="教务未审核" value="3"></el-option>
+				<el-option label="教务已审核" value="4"></el-option>
 			</el-select>
 			<el-button style="background-color: #5FB878;color: white; width: 50px;" size="mini" @click="selall">查询
 			</el-button>
@@ -18,22 +18,27 @@
 			<el-table :data="memorandumentData" border style="width: 100%">
 				<el-table-column prop="memorandumattachmentId" label="编号" width="50" align="center">
 				</el-table-column>
-				<el-table-column prop="register.consultant" label="学生姓名" width="250" align="center">
+				<el-table-column prop="register.consultant" label="学生姓名" width="160" align="center">
 				</el-table-column>
-				<el-table-column prop="register.sex" label="性别" width="250" align="center">
+				<el-table-column prop="register.sex" label="性别" width="160" align="center">
 				</el-table-column>
-				<el-table-column prop="register.phone" label="联系方式" width="250" align="center">
+				<el-table-column prop="register.phone" label="联系方式" width="160" align="center">
 				</el-table-column>
-				<el-table-column prop="zsisexamine" label="招生审核状态" width="250" align="center">
+				<el-table-column prop="zsisexamine" label="招生审核状态" width="200" align="center">
 					<template v-slot="slot">
 						<p v-if="slot.row.zsisexamine==0">未审核</p>
 						<p v-if="slot.row.zsisexamine==1">已审核</p>
 					</template>
 				</el-table-column>
+				<el-table-column prop="jwisexamine" label="教务审核状态" width="200" align="center">
+					<template v-slot="slot">
+						<p v-if="slot.row.jwisexamine==0">未审核</p>
+						<p v-if="slot.row.jwisexamine==1">已审核</p>
+					</template>
+				</el-table-column>
 				<el-table-column fixed="right" label="操作" align="center">
 					<template #default="scope">
-						<el-button @click="upzsisexamine(scope.row)" type="text" size="small">招生审核</el-button>
-						<!-- 	<el-button type="text" size="small" @click="upjwisexamine(scope.row)">教务审核</el-button> -->
+						<el-button type="text" size="small" @click="upjwisexamine(scope.row)">教务审核</el-button>
 					</template>
 				</el-table-column>
 			</el-table>
@@ -60,11 +65,11 @@
 	} from 'element-plus'
 	export default {
 		methods: {
-			//修改招生审核
-			upzsisexamine(row) {
+			upjwisexamine(row) {
 				const _this = this
-				row.zsisexamine = 1
-				this.axios.put("http://localhost:8089/tsm/upzsisexamine", row, {
+				//修改教务
+				row.jwisexamine = 1
+				this.axios.put("http://localhost:8089/tsm/upjwisexamine", row, {
 						headers: {
 							'content-type': 'application/json',
 							'jwtAuth': _this.$store.getters.token
@@ -73,6 +78,31 @@
 					.then(function(response) { // eslint-disable-line no-unused-vars
 						_this.selall()
 					})
+				/* 传咨询登记的id去后台 */
+				this.axios.get("http://localhost:8089/tsm/selectsouid?registerId=" + row.registerId, {
+						headers: {
+							'content-type': 'application/json',
+							'jwtAuth': _this.$store.getters.token
+						}
+					})
+					.then(function(response) {
+						_this.registerdata = response.data
+						console.log(response)
+					}).catch(function(error) {
+						console.log(error)
+					})
+				
+			/* 	this.axios.post("http://localhost:8089/tsm/addstudent", this.student, {
+						headers: {
+							'content-type': 'application/json',
+							'jwtAuth': _this.$store.getters.token
+						}
+					})
+					.then(function(response) { // eslint-disable-line no-unused-vars
+						console.log(response)
+					}).catch(function(error) {
+						console.log(error)
+					}) */
 			},
 			//模糊查询
 			selall() {
@@ -108,9 +138,41 @@
 						}).catch(function(error) {
 							console.log(error)
 						})
+				} else if (this.select == 3) { // eslint-disable-line no-unused-vars
+					const _this = this
+					this.axios.get("http://localhost:8089/tsm/wshjwisexamine", {
+							params: this.pageInfo,
+							headers: {
+								'content-type': 'application/json',
+								'jwtAuth': _this.$store.getters.token
+							}
+						})
+						.then(function(response) {
+							_this.memorandumentData = response.data.list
+							_this.pageInfo.total = response.data.total
+							console.log(response)
+						}).catch(function(error) {
+							console.log(error)
+						})
+				} else if (this.select == 4) { // eslint-disable-line no-unused-vars
+					const _this = this
+					this.axios.get("http://localhost:8089/tsm/yshjwisexamine", {
+							params: this.pageInfo,
+							headers: {
+								'content-type': 'application/json',
+								'jwtAuth': _this.$store.getters.token
+							}
+						})
+						.then(function(response) {
+							_this.memorandumentData = response.data.list
+							_this.pageInfo.total = response.data.total
+							console.log(response)
+						}).catch(function(error) {
+							console.log(error)
+						})
 				} else { // eslint-disable-line no-unused-vars
 					const _this = this
-					this.axios.get("http://localhost:8089/tsm/seleAllmemorandumatt", {
+					this.axios.get("http://localhost:8089/tsm/seleAllmemorandumatt1", {
 							params: this.pageInfo,
 							headers: {
 								'content-type': 'application/json',
@@ -127,7 +189,6 @@
 				}
 			},
 			handleSizeChange(pagesize) {
-				//var _this = this
 				this.pageInfo.pagesize = pagesize
 				var ps = qs.stringify(this.pageInfo) // eslint-disable-line no-unused-vars
 				this.selall()
@@ -143,13 +204,6 @@
 		},
 		data() {
 			return {
-				jwsh() {
-					ElMessage({
-						showClose: true,
-						message: '招生人员未审核!',
-						type: 'error'
-					});
-				},
 				deld() {
 					ElMessage({
 						showClose: true,
@@ -164,6 +218,7 @@
 						type: 'success'
 					});
 				},
+				student: {},
 				pageInfo: {
 					currentPage: 1, //当前页数，由用户指定
 					pagesize: 3, //每页显示的条数
@@ -181,14 +236,12 @@
 				search: ref(''),
 				value: '',
 				memorandumentData: [],
-				returnvisitdate: [],
+				registerdata: [],
 			}
 		},
 		created() {
-			//主键
 			const _this = this
 			this.selall()
-			//外键
 			this.axios.get("http://localhost:8089/tsm/Wjselectreturnvisit")
 				.then(function(response) {
 					_this.returnvisitdate = response.data
