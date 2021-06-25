a8914b163ba6a6a7c757780fa35ade95843b6436
ZedBoot <ZedBoot@163.com>
2021-06-25 16:05:28 +0800

    aa

--- TSM/src/components/student/DropOut.vue
+++ TSM/src/components/student/DropOut.vue
@@ -114,7 +114,8 @@
 				this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
 					confirmButtonText: '确定',
 					cancelButtonText: '取消',
-					type: 'warning'
+					type: 'warning',
+					
 				}).then(() => {
 					console.log(row);
 					_this.axios.put("http://localhost:8089/tsm/deleteDrop", row, {
